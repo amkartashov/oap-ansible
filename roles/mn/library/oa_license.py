d047bf8d05ad22aeb10804825408e9348f2c4d38
Andrey Kartashov <gorilych@gmail.com>
2017-09-06 00:27:46 +0700

    Module mn/oa_license: added documentation string

--- roles/mn/library/oa_license.py
+++ roles/mn/library/oa_license.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python
+# Copyright (c) Andrey Kartashov
+# see LICENSE file
 
-import os
+DOCUMENTATION = '''
+---
+module: oa_license
+short_description: installs OAP license
+description: Installs OAP license from local file on target host to OA management node.
+version_added: "2.3.2"
+author: Andrey Kartashov (gorilych@gmail.com)
+options:
+    license_file:
+        description: path to license file (local)
+        required: true
+        type: path
+requirements:
+    ansible: 2.3
+notes: uses library oaapi, embedded into module_utils
+'''
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+    'status': ['preview'],
+    'supported_by': 'community'}
 
 from ansible.module_utils.basic import AnsibleModule
 
