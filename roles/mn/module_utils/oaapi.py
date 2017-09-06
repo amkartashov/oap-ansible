46e36f78db7038a64724c0a0bec865c9be418095
Andrey Kartashov <gorilych@gmail.com>
2017-09-07 02:14:57 +0700

    Module utils mn/oaapi added aps_type_generator()

--- roles/mn/module_utils/oaapi.py
+++ roles/mn/module_utils/oaapi.py
@@ -2,17 +2,106 @@
 
 import os
 import time
+import json
+import hashlib
 import re
 import xmlrpclib
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import ParseError
 from urllib2 import HTTPError
+from types import BooleanType, IntType, FloatType, StringType, ListType
+from copy import deepcopy
 
 import poaupdater.uLogging
 from poaupdater import openapi
 from poaupdater import apsapi
 from poaupdater.openapi import OpenAPIError
 from poaupdater.uConfig import Config
+
+
+primitive_types = { 
+    'boolean': IntType, # we can't subclass Boolean
+    'integer': IntType,
+    'number': FloatType,
+    'string': StringType
+    }                                                                
+
+# memoization: mapping schema md5sum -> type
+aps_types = dict()
+
+def aps_type_generator(get_aps_type_schema,
+        typeid=None, 
+        schema=None, 
+        name=None,
+        parent_typeid=None,
+        ):
+    global aps_types
+    # either type id or schema should be specified
+    if typeid:
+        schema = get_aps_type_schema(typeid)
+    # schema is either json string or json object (dict)
+    typejson = schema if isinstance(schema, dict) else json.loads(schema)
+    schema_md5 = hashlib.md5(json.dumps(typejson, sort_keys=True)).hexdigest()
+    if schema_md5 in aps_types: # met this type before
+        return aps_types[schema_md5]
+    typetype = typejson.get('type', 'object')
+    name = name or typejson.get('name', typetype)
+    if typetype in primitive_types:
+        t = type(
+                str(name + schema_md5),
+                (primitive_types[typetype],),
+                {
+                    '_aps_type_schema': typejson,
+                    '_aps_type_schema_md5': schema_md5,
+                    '_aps_type_name': name,
+                    '_aps_type_id': name,
+                })
+        aps_types[schema_md5] = t
+    elif typetype == 'array':
+        itemstype = aps_type_generator(get_aps_type_schema, 
+                schema=typejson['items'], parent_typeid=parent_typeid or typeid)
+        typetype = 'array of ' + itemstype._aps_type_id
+        name = name or typejson.get('name', typetype)
+        t = type(
+                str(name + schema_md5),
+                (ListType,),
+                { 
+                    '_aps_type_schema': typejson,
+                    '_aps_type_schema_md5': schema_md5,
+                    '_aps_type_name': name,
+                    '_aps_type_id': name,
+                    '_itemstype': itemstype,
+                })
+        aps_types[schema_md5] = t
+    elif typetype == 'object':
+        typeid = typejson.get('id')
+        implements = typejson.get('implements')
+        properties = typejson.get('properties')
+        structures = typejson.get('structures')
+        basetypes = list()
+        if implements:
+            for t in implements:
+                basetypes.append(aps_type_generator(get_aps_type_schema, t))
+        typeproperties = {
+                '_aps_type_schema': typejson,
+                '_aps_type_schema_md5': schema_md5,
+                '_aps_type_name': name,
+                '_aps_type_id': typeid,
+                }
+        if properties:
+            for prop in properties:
+                typeproperties[prop] = aps_type_generator(get_aps_type_schema, 
+                        schema=properties[prop],
+                        parent_typeid=parent_typeid or typeid
+                        )
+        t = type(str(name + schema_md5), tuple(basetypes), typeproperties)
+        aps_types[schema_md5] = t
+    elif typetype.startswith('http'): # reference to another type
+        t = aps_type_generator(get_aps_type_schema, typetype)
+    else: # typetype is structure in parent type
+        t = aps_type_generator(get_aps_type_schema, parent_typeid+'#'+typetype, parent_typeid=parent_typeid)
+    return t
+
 
 class OaError(Exception):
     def __init__(self, msg=None, innerexc=None):
@@ -206,5 +295,13 @@ class OaApi:
 
     def get_aps_type_schema(self, typeid):
         res = self.GET.types(rql='?id='+typeid)
-        return res[0] if res else None
+        if not res:
+            return None
+        res = res[0]._dump_()
+        if '#' in typeid:
+            structure_name = typeid.split('#')[1]
+            res = res['structures'][structure_name]
+            res['name'] = structure_name
+            res['id'] = typeid
+        return res
 
